cba18e145d530d9f5b365aa22e71a15be1f155b1
beauthebeau <beau@beauthebeau.pro>
2023-12-24 16:47:46 +0000

    feat: Create failsafe

--- hold-to-lean/mouse_listener.py
+++ hold-to-lean/mouse_listener.py
@@ -1,7 +1,10 @@
+import time
+from threading import Thread
 import logging
-from pynput import mouse
 import pyautogui
 
+from pynput import mouse
+from pynput.mouse import Button, Controller as MouseController
 
 def press_e():
     pyautogui.press('e')
@@ -12,23 +15,44 @@ def press_q():
 
 
 class MouseListener:
-    # config is optional
+
     def __init__(self, config=None):
         self.held_buttons = set()
 
         if config['left'] == config['right']:
-            raise Exception("Left and right buttons cannot be the same")
+            raise ValueError("Left and right buttons cannot be the same! Please reconfigure.")
 
         if config is None:
             config = {"left": "Button.button9", "right": "Button.button8"}
 
+        self.failsafe_active = False
+        self.failsafe_thread = Thread(target=self.check_failsafe)
+        self.failsafe_thread.daemon = True
+
         self.button_actions = {
             config['left']: press_e,
-            config['right']: press_q
+            config['right']: press_q,
+            'Button.middle': self.activate_failsafe
         }
         self.listener = mouse.Listener(on_click=self.on_click)
 
+    def check_failsafe(self):
+        while True:
+            time.sleep(0.1)  # Adjust the interval as needed
+            if self.failsafe_active:
+                logging.warning("Failsafe triggered - Exiting")
+                # Perform any necessary cleanup actions here
+                break
+
+    def activate_failsafe(self):
+        self.failsafe_active = True
+
+    def deactivate_failsafe(self):
+        self.failsafe_active = False
+
     def on_click(self, x, y, button, pressed):
+        if self.failsafe_active:
+            return
         button = str(button)
 
         if pressed:
@@ -52,6 +76,7 @@ class MouseListener:
                     logging.error(f"Error executing action for {button}: {e}")
 
     def start(self):
+        self.failsafe_thread.start()
         self.listener.start()
 
     def join(self):
